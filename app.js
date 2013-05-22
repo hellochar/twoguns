3b872cf3ea591db2f9811324a05465067e42022d
hellochar <hellocharlien@hotmail.com>
2013-05-22 04:55:45 -0600

    add everything

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+  , app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+app.configure(function() {
+    app.use(express.static(__dirname + '/public'));
+});
+
+
+server.listen(80);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
