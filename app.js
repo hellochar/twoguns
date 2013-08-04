9d056b2b89930432668fc7f9fa667adfd27b3d07
hellochar <hellocharlien@hotmail.com>
2013-08-03 21:51:44 -0700

    port is a variable

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var express = require('express')
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
+PORT = 3000;
+
 app.configure(function() {
     app.use(express.static(__dirname + '/public'));
 });
 
 
-server.listen(80);
+server.listen(PORT);
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
